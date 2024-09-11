b29324c34c0c09f858578c7a5832b913000db807
Gabriel Melo <gabrielmelo1992@gmail.com>
2024-09-11 17:39:54 -0300

    list projects working

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Domain\Repositories\ClientRepositoryInterface;
 use App\Infrastructure\Repositories\EloquentClientRepository;
+use App\Domain\Repositories\ProjectRepositoryInterface;
+use App\Infrastructure\Repositories\EloquentProjectRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // Bind the ClientRepositoryInterface to EloquentClientRepository
         $this->app->bind(ClientRepositoryInterface::class, EloquentClientRepository::class);
+
+        // Bind the ProjectRepositoryInterface to EloquentProjectRepository
+        $this->app->bind(ProjectRepositoryInterface::class, EloquentProjectRepository::class);
     }
 
     public function boot()
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
-// use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\UtilityController;
 
 Route::get('/', function () {
@@ -26,10 +26,10 @@ Route::prefix('clients')->group(function () {
     Route::delete('/{id}', [ClientController::class, 'destroy']); // Delete a client
 });
 
-// Route::prefix('projects')->group(function () {
-//     Route::post('/', [ProjectController::class, 'store']); // Create a new project
-//     Route::get('/', [ProjectController::class, 'index']); // List all projects
-//     Route::get('/{id}', [ProjectController::class, 'show']); // Show a specific project
-//     Route::put('/{id}', [ProjectController::class, 'update']); // Update a project
-//     Route::delete('/{id}', [ProjectController::class, 'destroy']); // Delete a project
-// });
+Route::prefix('projects')->group(function () {
+    Route::post('/', [ProjectController::class, 'store']); // Create a new project
+    Route::get('/', [ProjectController::class, 'index']); // List all projects
+    Route::get('/{id}', [ProjectController::class, 'show']); // Show a specific project
+    Route::put('/{id}', [ProjectController::class, 'update']); // Update a project
+    Route::delete('/{id}', [ProjectController::class, 'destroy']); // Delete a project
+});
